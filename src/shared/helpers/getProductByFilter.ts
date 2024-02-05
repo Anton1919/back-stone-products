286b9f73ac5eaa577f465e36f176eec7ca346eca
Anton1919 <bf3kenzo@mail.ru>
2024-02-05 16:10:07 +0300

    refactoring of functions

---
+++ src/shared/helpers/getProductByFilter.ts
@@ -0,0 +1,20 @@
+import { productCollection } from '../../repositories/db';
+
+type CharacteristicsType = Record<string, any>;
+
+export async function getProductByFilter(
+    sortBy: string,
+    pageNum: number,
+    pageSize: number,
+    filterProductType?: CharacteristicsType,
+) {
+    const filter = filterProductType ? filterProductType : {};
+    const sortOptions: Record<string, any> = {};
+    sortOptions['price'] = sortBy === 'desc' ? -1 : 1;
+    return productCollection
+        .find(filter)
+        .sort(sortOptions)
+        .skip((pageNum - 1) * pageSize)
+        .limit(pageSize)
+        .toArray();
+}
